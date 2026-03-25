1f09e1fd3aa24f4248f48b4a11d09bc60553917e
dewmiamaya <dewmiamaya75@gmail.com>
2026-03-25 18:01:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/dewmiamaya/Titan79/refs/heads/main/images/20260323_102432.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 MALSHAN-MD Is Alive Now😍*",
+BOT_OWNER: '94765900102',  // Replace with the owner's phone number
 
 
 
