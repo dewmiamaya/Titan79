d8ece8c92c5e6743a41243adde069222435bf528
dewmiamaya <dewmiamaya75@gmail.com>
2026-03-28 17:45:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,14 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/dewmiamaya/Titan79/refs/heads/main/images/20260323_102432.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 MALSHAN-MD Is Alive Now😍*",
+ALIVE_MSG: process.env.ALIVE_MSG || `🇬🇧 *Welcome to MALSHAN-MD Ai and multimedia services.* (To view commands, send .MENU📄 )
+
+🇰🇷 *MALSHAN-MD AI 및 멀티미디어 서비스에 오신 것을 환영합니다.*  (명령어를 보려면 .MENU를 보내세요📄 )
+
+🇱🇰 *MALSHAN-MD Ai and multimedia Services වෙතට ඔබව සාදරයෙන් පිළිගනිමු* (විධාන බැලීමට .MENU ලෙස එවන්න📄)
+
+> ⚡ Powered by SLPR MALSHAN 📡
+> 🚀 Fast • Smart • Reliable 🛡️`,
 BOT_OWNER: '94765900102',  // Replace with the owner's phone number
 
 
