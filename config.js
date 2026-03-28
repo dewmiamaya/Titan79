c0a99d4a548ebb24725a278fcd42e4cb6827f9f0
dewmiamaya <dewmiamaya75@gmail.com>
2026-03-29 02:13:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "3wQhHaYC#--BmUU16BLYYzQqbwMcXA1MM09mVyt8AIQMtEcpiThA",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/dewmiamaya/Titan79/refs/heads/main/images/20260328_175039.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || `🇬🇧 *Welcome to MALSHAN-MD Ai and multimedia services.* (To view commands, send .MENU📄 )
 
